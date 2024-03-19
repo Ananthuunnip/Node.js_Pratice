349a690730011ae2575077b00a61cd0b8792ed92
Ananthuunnip <ananthu2929@gmail.com>
2024-03-19 10:41:02 -0700

    request created

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const http = require("http");
 
 const server = http.createServer((req,res)=>{
     console.log(req.url,req.method,req.headers)
-    console.log("ANANTHU UNNI P")
-    res.end("ANANTHU UNNI P")
+    if(req.url == "/home"){
+        res.write("<html>");
+        res.write("<head><title>Instagram</title><head>");
+        res.write("<body><h1>Welcome home</h1></body>");
+        res.write("</html>");
+        res.end();
+
+        
+    }else if (req.url == "/about"){
+        res.write("<html>");
+        res.write("<head><title>Instagram</title><head>");
+        res.write("<body><h1>Welcome to About Us page</h1></body>");
+        res.write("</html>");
+        res.end();
+    }else if (req.url == "/node"){
+        res.write("<html>");
+        res.write("<head><title>Instagram</title><head>");
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+        res.write("</html>");
+        res.end();
+    }else if (req.url == "/" || req.url == " "){
+        res.write("<html>");
+        res.write("<head><title>Instagram</title><head>");
+        res.write("<body><h1>ANANTHU UNNI P</h1></body>");
+        res.write("</html>");
+        res.end();
+    }
     
 
 });
