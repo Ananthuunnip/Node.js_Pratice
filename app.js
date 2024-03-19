40c05c64757e637fe459cfd7b4fc38ea15971736
Ananthuunnip <ananthu2929@gmail.com>
2024-03-19 14:04:26 -0700

    request redirecting

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers)
-    if(req.url == "/home"){
+    const url = req.url; 
+    const method = req.method;
+    if (url ==="/"){
         res.write("<html>");
         res.write("<head><title>Instagram</title><head>");
-        res.write("<body><h1>Welcome home</h1></body>");
+        res.write("<body><form action='/message' method='POST'><label>Enter Name</label><br><input type='text' name ='message'><button type='submit'>submit</button></form></body>");
         res.write("</html>");
-        res.end();
-
-        
-    }else if (req.url == "/about"){
-        res.write("<html>");
-        res.write("<head><title>Instagram</title><head>");
-        res.write("<body><h1>Welcome to About Us page</h1></body>");
-        res.write("</html>");
-        res.end();
-    }else if (req.url == "/node"){
-        res.write("<html>");
-        res.write("<head><title>Instagram</title><head>");
-        res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-        res.write("</html>");
-        res.end();
-    }else if (req.url == "/" || req.url == " "){
-        res.write("<html>");
-        res.write("<head><title>Instagram</title><head>");
-        res.write("<body><h1>ANANTHU UNNI P</h1></body>");
-        res.write("</html>");
-        res.end();
+        return res.end();
+    }
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync("message.txt", "DUMMY")
+            res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
     }
     
-
+    res.setHeader("Content-Type","text/html");
+    res.write("<html>");
+    res.write("<head><title>Instagram</title><head>");
+    res.write("<body><h1>ANANTHU UNNI P</h1></body>");
+    res.write("</html>");
+    res.end();
 });
 server.listen(4000)
