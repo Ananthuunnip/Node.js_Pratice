2d05c6b15bd7e9cc5f37a51f83f4ded231e53c6d
Ananthuunnip <ananthu2929@gmail.com>
2024-03-19 21:01:46 -0700

    add handler and routes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const routes = require("./routes")
-const server = http.createServer((req,res)=>{
-        
-});
+
+console.log(routes.someText)
+const server = http.createServer(routes.handeler);
 server.listen(3000)
